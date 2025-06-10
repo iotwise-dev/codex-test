d7bc232f62b56a62b37218374c33675b8ac955ad
Eduardo Gonzalez <egonz993@gmail.com>
2025-06-10 13:52:26 -0500

    refactor: receive path in fn parameters

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import base64
-import sys
-
 
 def ImageToBase64(image_path):
     """Print the base64 representation of the image at the given path."""
@@ -9,10 +7,5 @@ def ImageToBase64(image_path):
     encoded_str = encoded_bytes.decode("utf-8")
     print(encoded_str)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <image_path>")
-    else:
-        ImageToBase64(sys.argv[1])
+ImageToBase64('./image.jpg')
 
